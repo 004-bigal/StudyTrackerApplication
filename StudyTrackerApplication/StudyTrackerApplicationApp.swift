f21264cbca2a50bd4e6b988e6ece6bd8d73d925b
004-bigal <alikorgun@gmail.com>
2026-09-07 20:51:01 +1000

    Initial Commit

---
+++ StudyTrackerApplication/StudyTrackerApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyTrackerApplicationApp.swift
+//  StudyTrackerApplication
+//
+//  Created by Alik Orgun on 7/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct StudyTrackerApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
