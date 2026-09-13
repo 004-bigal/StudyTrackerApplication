ec9ac0019f0e586f6b1e2a405788a833005cb1cb
004-bigal <alikorgun@gmail.com>
2026-09-13 17:57:30 +1000

    feat: Added functionality to the repo implementation file.

--- StudyTrackerApplication/Repositories/StudyRepositoryImplementation.swift
+++ StudyTrackerApplication/Repositories/StudyRepositoryImplementation.swift
@@ -9,19 +9,19 @@ class StudyRepositoryImplementation {
     var sessionRecords: [SessionRecord] = []
     var studyTasks: [StudyTask] = []
     func saveASession(_ sessionRecord: SessionRecord) {
-        
+        sessionRecords.append(sessionRecord)
     }
-    func retrieveAllSessionData() {
-        
+    func retrieveAllSessionData() -> [SessionRecord] {
+        return sessionRecord
     }
-    func retrieveAllStudyTasks() {
-        
+    func retrieveAllStudyTasks() -> [StudyTask] {
+        return studyTasks
     }
     func addingTask(_ studyTask: StudyTask) {
-        
+        studyTask.append(StudyTask)
     }
     func deletingTask(_ studyTask: StudyTask) {
-        
+        studyTask.remove(StudyTask)
     }
     func updatingTask(_ studyTask: StudyTask) {
         
