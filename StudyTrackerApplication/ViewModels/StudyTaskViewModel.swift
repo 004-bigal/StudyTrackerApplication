582338cd35e17466fcf35cd6271c273ac920de43
004-bigal <alikorgun@gmail.com>
2026-09-13 23:42:23 +1000

    feat: Added basic code for StudyTaskViewModel.

--- StudyTrackerApplication/ViewModels/StudyTaskViewModel.swift
+++ StudyTrackerApplication/ViewModels/StudyTaskViewModel.swift
@@ -9,5 +9,10 @@ import Foundation
 import Combine
 
 class StudyTaskViewModel: ObservableObject {
+    @Published var studyTasks: [StudyTask] = []
+    let repository: StudyRepository
     
+    init(repository: StudyRepository) {
+        self.repository = repository
+    }
 }
