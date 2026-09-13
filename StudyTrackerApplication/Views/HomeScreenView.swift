5ce4add1aebeab53f6aa3166e195df71cbd72be7
004-bigal <alikorgun@gmail.com>
2026-09-13 14:00:10 +1000

    feat: Skeleton code added for HomeScreenView.

--- StudyTrackerApplication/Views/HomeScreenView.swift
+++ StudyTrackerApplication/Views/HomeScreenView.swift
@@ -5,3 +5,14 @@
 //  Created by Alik Orgun on 13/9/2026.
 //
 
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
