6973c4caf285267055d1d1fc04fbeff66b76c3f2
004-bigal <alikorgun@gmail.com>
2026-09-13 15:25:10 +1000

    feat: Added variables to the WeeklyStudyProgress model file. Included startDate, totalStudyTime and sessions, an array of SessionRecord objects.

--- StudyTrackerApplication/Models/WeeklyStudyProgress.swift
+++ StudyTrackerApplication/Models/WeeklyStudyProgress.swift
@@ -6,7 +6,16 @@
 //
 
 import SwiftData
+import Foundation
 
 @Model class WeeklyStudyProgress {
+    var startDate: Date
+    var totalStudyTime: Int
+    var sessions = [SessionRecord]()
     
+    init(startDate: Date, totalStudyTime: Int, sessions: [SessionRecord]) {
+        self.startDate = startDate
+        self.totalStudyTime = totalStudyTime
+        self.sessions = sessions
+    }
 }
