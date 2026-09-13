f42a09bf699abfb85af57ce26a85becc228df716
004-bigal <alikorgun@gmail.com>
2026-09-13 14:56:40 +1000

    feat: Added variables to the StudyTask model file. Included ID, taskName, isTaskDone and dateCreated. Also added an initialiser.

--- StudyTrackerApplication/Models/StudyTask.swift
+++ StudyTrackerApplication/Models/StudyTask.swift
@@ -5,7 +5,18 @@
 //  Created by Alik Orgun on 7/9/2026.
 //
 import SwiftData
+import Foundation
 
 @Model class StudyTask {
+    var id = UUID()
+    var taskName: String
+    var isTaskDone: Bool
+    var dateCreated = Date()
     
+    init(taskName: String, isTaskDone: Bool, dateCreated: Date) {
+        self.id = UUID()
+        self.taskName = taskName
+        self.isTaskDone = isTaskDone
+        self.dateCreated = dateCreated
+    }
 }
