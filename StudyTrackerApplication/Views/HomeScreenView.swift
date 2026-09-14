4623531495a9ac020285cd165301cbc63f357505
004-bigal <alikorgun@gmail.com>
2026-09-14 19:01:07 +1000

    feat: Added skeleton code to the HomeScreenView file.

--- StudyTrackerApplication/Views/HomeScreenView.swift
+++ StudyTrackerApplication/Views/HomeScreenView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct HomeScreenView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            NavigationLink("") {
+                
+            }
+            NavigationLink("") {
+                
+            }
+            NavigationLink("") {
+                
+            }
+        }
     }
 }
 
