2e4ccadf5a5b4c243cdeec7f167050de168a58d4
004-bigal <alikorgun@gmail.com>
2026-09-14 21:16:11 +1000

    feat: Added functionality for the session record view. Included fields to enable user to type in subject name & study duration in minutes. Log session is able to log the session in a list.

--- StudyTrackerApplication/Views/SessionRecordView.swift
+++ StudyTrackerApplication/Views/SessionRecordView.swift
@@ -10,13 +10,63 @@ import SwiftUI
 struct SessionRecordView: View {
     @State var subjectName: String = ""
     @State var studySessionDuration: String = ""
+    @State var errorMessage: String?
     let theViewModel = SessionRecordViewModel(repository: StudyRepositoryImplementation())
     var body: some View {
+        
+        if let errorMessage = errorMessage {
+            Text(errorMessage)
+                .foregroundStyle(.red)
+        }
+        
         VStack(spacing: 20) {
-            
+
+            TextField("Name of the subject", text: $subjectName)
+                .textFieldStyle(.roundedBorder)
+
+            TextField("Duration (minutes)", text: $studySessionDuration)
+                .keyboardType(.numberPad)
+                .textFieldStyle(.roundedBorder)
+
+            Button("Log session") { logStudySession() }
+            .buttonStyle(.borderedProminent)
+
+            VStack {
+                List(theViewModel.sessionRecord) { session in
+                    VStack(alignment: .leading) {
+                        Text(session.subjectName)
+                            .font(.headline)
+                        Text("Duration: \(session.studySessionDuration) min")
+                        Text("Date: \(session.sessionDate.formatted())")
+                            .font(.caption)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+        .padding()
+        .onAppear {
+            theViewModel.loadAllSessionData()
         }
     }
-}
+
+    func logStudySession() {
+        guard let duration = Int(studySessionDuration) else {
+            errorMessage = "The duration of the study session must be a number"
+            return
+        }
+
+        theViewModel.sessionLog(
+            subjectName: subjectName,
+            studySessionDuration: duration
+        )
+
+        subjectName = ""
+        studySessionDuration = ""
+        errorMessage = nil
+        }
+    }
 
 #Preview {
     SessionRecordView()
