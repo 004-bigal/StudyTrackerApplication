e03484b189d12556659588cd72ee2c0f7ea0b1cd
004-bigal <alikorgun@gmail.com>
2026-09-14 18:47:46 +1000

    feat: Added a progress load function to the WeeklyStudyProgressViewModel file.

--- StudyTrackerApplication/ViewModels/WeeklyStudyProgressViewModel.swift
+++ StudyTrackerApplication/ViewModels/WeeklyStudyProgressViewModel.swift
@@ -15,4 +15,19 @@ class WeeklyStudyProgressViewModel: ObservableObject {
     init(repository: StudyRepository) {
         self.repository = repository
     }
+    
+    func weeklyProgressLoad() {
+        let sessionsFromOverTheWeek = repository.retrieveAllSessionData()
+        guard let theWeek = Calendar.current.date(byAdding: .day, value: -7, to: Date()) else { return }
+        
+        let recentStudySessions = sessionsFromOverTheWeek.filter{ session in session.sessionDate >= theWeek }
+        
+        var totalStudy = 0
+        for session in recentStudySessions {
+            let studyMinutes = session.studySessionDuration
+            totalStudy = totalStudy + studyMinutes
+        }
+        
+        weeklyStudyProgress = [WeeklyStudyProgress(startDate: theWeek, totalStudyTime: totalStudy, sessions: sessionsFromOverTheWeek)]
+    }
 }
