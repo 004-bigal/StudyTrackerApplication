5c83d052b1446b8d7843f2b32b39cdc0fd077995
004-bigal <alikorgun@gmail.com>
2026-09-16 20:54:40 +1000

    feat: Added fake repo code to comply with repo code.

--- StudyTrackerApplication/Repositories/FakeRepository.swift
+++ StudyTrackerApplication/Repositories/FakeRepository.swift
@@ -5,3 +5,34 @@
 //  Created by Alik Orgun on 16/9/2026.
 //
 
+class FakeRepository: StudyRepository {
+    var sessionRecords: [SessionRecord] = []
+    var studyTasks: [StudyTask] = []
+
+    func saveASession(_ sessionRecord: SessionRecord) {
+        sessionRecords.append(sessionRecord)
+    }
+
+    func retrieveAllSessionData() -> [SessionRecord] {
+        sessionRecords
+    }
+    
+    func retrieveAllStudyTasks() -> [StudyTask] {
+        studyTasks
+    }
+
+    func addingTask(_ studyTask: StudyTask) {
+        studyTasks.append(studyTask)
+    }
+
+    func deletingTask(_ studyTask: StudyTask) {
+        studyTasks.removeAll { $0.id == studyTask.id }
+    }
+
+    func updatingTask(_ studyTask: StudyTask) {
+        if let index = studyTasks.firstIndex(where: { $0.id == studyTask.id }) {
+            studyTasks[index] = studyTask
+        }
+    }
+}
+
