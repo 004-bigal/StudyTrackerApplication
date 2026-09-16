5752180e72f4cca2e7163a3ddccb20442f7ed6fb
004-bigal <alikorgun@gmail.com>
2026-09-16 10:26:02 +1000

    feat: Added studytaskerror enum.

--- StudyTrackerApplication/Error/AppDomainErrors.swift
+++ StudyTrackerApplication/Error/AppDomainErrors.swift
@@ -4,4 +4,15 @@
 //
 //  Created by Alik Orgun on 16/9/2026.
 //
+import Foundation
 
+enum StudyTaskErrors: LocalizedError {
+    case emptyName
+
+    var studyTaskErrorDescription: String? {
+        switch self {
+        case .emptyName:
+            return "The task name cannot be empty."
+        }
+    }
+}
