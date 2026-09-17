9d0f52ae66fd3dc32aca7b92c65b4746d24009ca
004-bigal <alikorgun@gmail.com>
2026-09-17 14:04:57 +1000

    docs: Added documentation to the error file.

--- StudyTrackerApplication/Error/AppDomainErrors.swift
+++ StudyTrackerApplication/Error/AppDomainErrors.swift
@@ -6,9 +6,14 @@
 //
 import Foundation
 
+// this file contains the code for errors in this app
+
+// this enum here deals with errors for creating a study task
 enum StudyTaskErrors: LocalizedError {
     case emptyName
 
+    // this provides a clear error message for the user
+    // in this case, it displays a clear error message for an empty task name
     var errorDescription: String? {
         switch self {
         case .emptyName:
@@ -17,10 +22,13 @@ enum StudyTaskErrors: LocalizedError {
     }
 }
 
+// this enum here deals with any errors in recording a study session
 enum SesisonRecordError: LocalizedError {
     case subjectNameMissing
     case studyTimeDurationMissing
     
+    // provides a clear error message for the user
+    // in this case here, the code here makes the app display missing field error messages
     var errorDescription: String? {
         switch self {
         case .subjectNameMissing:
