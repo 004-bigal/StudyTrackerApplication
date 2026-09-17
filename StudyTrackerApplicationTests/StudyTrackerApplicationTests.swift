afe2d7585d81bb2f3781e6949719f33bac1e1877
004-bigal <alikorgun@gmail.com>
2026-09-17 12:48:30 +1000

    chore: polished up indentation.

--- StudyTrackerApplicationTests/StudyTrackerApplicationTests.swift
+++ StudyTrackerApplicationTests/StudyTrackerApplicationTests.swift
@@ -11,54 +11,54 @@ import XCTest
 final class StudyTrackerApplicationTests: XCTestCase {
 
     func test_createTask_succeeds_withValidName() {
-            let studyRepository = FakeRepository()
-            let useCase = CreateStudyTaskUseCase(studyRepository: studyRepository)
+        let studyRepository = FakeRepository()
+        let useCase = CreateStudyTaskUseCase(studyRepository: studyRepository)
 
-            XCTAssertNoThrow(try useCase.executeCreateStudyTaskUseCase(taskName: "Programming revision"))
-            XCTAssertEqual(studyRepository.studyTasks.count, 1)
+        XCTAssertNoThrow(try useCase.executeCreateStudyTaskUseCase(taskName: "Programming revision"))
+        XCTAssertEqual(studyRepository.studyTasks.count, 1)
+    }
+
+    func test_createTask_fails_whenNameIsEmpty() {
+        let studyRepository = FakeRepository()
+        let useCase = CreateStudyTaskUseCase(studyRepository: studyRepository)
+
+        XCTAssertThrowsError(try useCase.executeCreateStudyTaskUseCase(taskName: "")) { error in
+            XCTAssertEqual(error as? StudyTaskErrors, .emptyName)
         }
+    }
 
-        func test_createTask_fails_whenNameIsEmpty() {
-            let studyRepository = FakeRepository()
-            let useCase = CreateStudyTaskUseCase(studyRepository: studyRepository)
+    func test_logSession_succeeds_withValidDuration() {
+        let studyRepository = FakeRepository()
+        let useCase = LogStudySessionUseCase(studyRepository: studyRepository)
 
-            XCTAssertThrowsError(try useCase.executeCreateStudyTaskUseCase(taskName: "")) { error in
-                XCTAssertEqual(error as? StudyTaskErrors, .emptyName)
-            }
+        XCTAssertNoThrow(try useCase.executeLogStudySessionUseCase(subjectName: "Mathematics 1", studyTimeDuration: 25))
+        XCTAssertEqual(studyRepository.sessionRecords.count, 1)
+    }
+
+    func test_logSession_fails_whenDurationIsZero() {
+        let studyRepository = FakeRepository()
+        let useCase = LogStudySessionUseCase(studyRepository: studyRepository)
+
+        XCTAssertThrowsError(try useCase.executeLogStudySessionUseCase(subjectName: "Advanced C++ Dev", studyTimeDuration: 0)) { error in
+            XCTAssertEqual(error as? SesisonRecordError, .studyTimeDurationMissing)
         }
+    }
 
-        func test_logSession_succeeds_withValidDuration() {
-            let studyRepository = FakeRepository()
-            let useCase = LogStudySessionUseCase(studyRepository: studyRepository)
+    func test_logSession_fails_whenSubjectNameIsEmpty() {
+        let studyRepository = FakeRepository()
+        let useCase = LogStudySessionUseCase(studyRepository: studyRepository)
 
-            XCTAssertNoThrow(try useCase.executeLogStudySessionUseCase(subjectName: "Mathematics 1", studyTimeDuration: 25))
-            XCTAssertEqual(studyRepository.sessionRecords.count, 1)
+        XCTAssertThrowsError(try useCase.executeLogStudySessionUseCase(subjectName: "", studyTimeDuration: 30)) { error in
+            XCTAssertEqual(error as? SesisonRecordError, .subjectNameMissing)
         }
-
-        func test_logSession_fails_whenDurationIsZero() {
-            let studyRepository = FakeRepository()
-            let useCase = LogStudySessionUseCase(studyRepository: studyRepository)
-
-            XCTAssertThrowsError(try useCase.executeLogStudySessionUseCase(subjectName: "Advanced C++ Dev", studyTimeDuration: 0)) { error in
-                XCTAssertEqual(error as? SesisonRecordError, .studyTimeDurationMissing)
-            }
-        }
-
-        func test_logSession_fails_whenSubjectNameIsEmpty() {
-            let studyRepository = FakeRepository()
-            let useCase = LogStudySessionUseCase(studyRepository: studyRepository)
-
-            XCTAssertThrowsError(try useCase.executeLogStudySessionUseCase(subjectName: "", studyTimeDuration: 30)) { error in
-                XCTAssertEqual(error as? SesisonRecordError, .subjectNameMissing)
-            }
-        }
+    }
         
-        func test_weeklyProgress_returnsEmptyWhenNoSessions() {
-            let studyRepository = FakeRepository()
-            let useCase = GenerateWeeklyProgressUseCase(studyRepository: studyRepository)
+    func test_weeklyProgress_returnsEmptyWhenNoSessions() {
+        let studyRepository = FakeRepository()
+        let useCase = GenerateWeeklyProgressUseCase(studyRepository: studyRepository)
 
-            let result = useCase.executeGenerateWeeklyProgressUseCase()
+        let result = useCase.executeGenerateWeeklyProgressUseCase()
 
-            XCTAssertEqual(result.count, 0)
-        }
+        XCTAssertEqual(result.count, 0)
+    }
 }
