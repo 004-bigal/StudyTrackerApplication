4e99c4d2f5c9ac2680ab96cd53e99850e0c6ed49
004-bigal <alikorgun@gmail.com>
2026-09-17 16:12:02 +1000

    docs: Added documentation.

--- StudyTrackerApplication/Views/HomeScreenView.swift
+++ StudyTrackerApplication/Views/HomeScreenView.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 
+// the home screen view for the application obviously
 struct HomeScreenView: View {
     let sharedRepository = StudyRepositoryImplementation()
     var body: some View {
+        // links to all three other screens
         NavigationStack {
             VStack(spacing: 20) {
                 NavigationLink("Log a study session") {
