92f73099c63c52f985dfebc599f44e8d36e94f72
004-bigal <alikorgun@gmail.com>
2026-09-17 14:07:57 +1000

    docs: Added documentation for the weekly study progress model file.

--- StudyTrackerApplication/Models/WeeklyStudyProgress.swift
+++ StudyTrackerApplication/Models/WeeklyStudyProgress.swift
@@ -8,11 +8,13 @@
 import SwiftData
 import Foundation
 
+// this creates the weekly study progress model
 @Model class WeeklyStudyProgress {
     var startDate: Date
     var totalStudyTime: Int
     var sessions = [SessionRecord]()
     
+    // initialiser
     init(startDate: Date, totalStudyTime: Int, sessions: [SessionRecord]) {
         self.startDate = startDate
         self.totalStudyTime = totalStudyTime
